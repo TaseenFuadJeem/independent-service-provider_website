368663b8a99a4e64830d3a33ba6e9fc41bca236f
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-16 21:48:12 +0600

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import AboutMe from './Pages/AboutMe/AboutMe';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import MyWorks from './Pages/MyWorks/MyWorks';
+import Navbar from './Pages/Navbar/Navbar';
 import NotFound from './Pages/NotFound/NotFound';
+import Services from './Pages/Services/Services';
 import Signup from './Pages/Signup/Signup';
 
 function App() {
   return (
     <div>
+
+      <Navbar></Navbar>
 
       <Routes>
 
@@ -18,7 +23,9 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/aboutme' element={<AboutMe></AboutMe>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='/my-works' element={<MyWorks></MyWorks>}></Route>
+        <Route path='/about-me' element={<AboutMe></AboutMe>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
---
+++ src/Pages/MyWorks/MyWorks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyWorks = () => {
+    return (
+        <div>
+            <h1>This is My work</h1>
+        </div>
+    );
+};
+
+export default MyWorks;
