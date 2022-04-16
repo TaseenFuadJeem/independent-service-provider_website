ab1e30dd9a1d8340aa7ead66815e6b61be84e0af
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-16 21:29:28 +0600

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AboutMe from './Pages/AboutMe/AboutMe';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Signup from './Pages/Signup/Signup';
 
 function App() {
   return (
     <div>
 
+      <Routes>
 
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/aboutme' element={<AboutMe></AboutMe>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+
+      </Routes>
 
     </div>
   );
