80488cce99fc893e305e082d5899d60360379338
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-17 00:47:17 +0600

    Fixed React class issues

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                             My name is Steve Rogers, and I am a beautiful mosaic of lived-experience and culture. Everyday I wake up in search of courage and authentic expression. And on this journey, I've been met with opportunities to create impact in ways I'd never imagine. January 2018 I became the best youngest photographer of the year. I got the global media platform that empowers creativity with tools to experience the beauty of the world. From 2014, i have learned a lot about photography. I studied about photography and also i researched a lot about photography.  I believed, everyone came with great gifts. People should find their own and I come with a great gift, and I will continue carry forward with the photography craft.
                         </p>
 
-                        <button class=" bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">
+                        <button className=" bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">
                             HIRE ME
                         </button>
 
@@ -26,6 +26,11 @@ const Home = () => {
                 </div>
 
             </div>
+
+            <div>
+                <h1 className='text-center text-2xl'>Explore my works</h1>
+            </div>
+
         </div>
     );
 };
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -5,29 +5,29 @@ const Signup = () => {
     return (
         <div>
 
-            <div class="h-screen bg-gradient-to-br from-blue-400 to-indigo-500 flex justify-center items-center w-full">
+            <div className="h-screen bg-gradient-to-br from-blue-400 to-indigo-500 flex justify-center items-center w-full">
                 <form>
-                    <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
-                        <div class="space-y-4">
-                            <h1 class="text-center text-2xl font-semibold text-gray-600">Register</h1>
+                    <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
+                        <div className="space-y-4">
+                            <h1 className="text-center text-2xl font-semibold text-gray-600">Register</h1>
                             <div>
-                                <label for="username" class="block mb-1 text-gray-600 font-semibold">Username</label>
-                                <input type="text" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                                <label htmlFor="username" className="block mb-1 text-gray-600 font-semibold">Username</label>
+                                <input type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                             </div>
                             <div>
-                                <label for="email" class="block mb-1 text-gray-600 font-semibold">Email</label>
-                                <input type="email" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                                <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">Email</label>
+                                <input type="email" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                             </div>
                             <div>
-                                <label for="password" class="block mb-1 text-gray-600 font-semibold">Password</label>
-                                <input type="password" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                                <label htmlFor="password" className="block mb-1 text-gray-600 font-semibold">Password</label>
+                                <input type="password" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                             </div>
                         </div>
-                        <button class="mt-4 w-full bg-gradient-to-tr bg-indigo-500 text-white py-2 rounded-md text-lg tracking-wide">Register</button>
+                        <button className="mt-4 w-full bg-gradient-to-tr bg-indigo-500 text-white py-2 rounded-md text-lg tracking-wide">Register</button>
 
                         <p className='text-center my-2'>Or</p>
 
-                        <button class="w-full bg-gradient-to-tr bg-indigo-500  text-white py-2 rounded-md text-lg tracking-wide">Continue with Google</button>
+                        <button className="w-full bg-gradient-to-tr bg-indigo-500  text-white py-2 rounded-md text-lg tracking-wide">Continue with Google</button>
 
                         <p className='text-center mt-3'>Already have an account? <Link className='text-indigo-700 font-bold' to="/login">Log in</Link></p>
 
