10b852f6ac9cb6ef3b99a560b5f74965c9144114
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-18 23:20:05 +0600

    Added style in search bar in gallery

--- src/Pages/ImageCard/ImageSearch.js
+++ src/Pages/ImageCard/ImageSearch.js
@@ -11,9 +11,9 @@ const ImageSearch = ({ searchText }) => {
     return (
         <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
             <form onSubmit={onSubmit} className="w-full max-w-sm">
-                <div className="flex items-center border-b-2 border-purple-500 py-2">
+                <div className="flex items-center border-b-2 border-indigo-500 py-2">
                     <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image..." />
-                    <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                    <button className="flex-shrink-0 bg-indigo-500 hover:bg-indigo-700 border-indigo-500 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                         Search
                     </button>
                 </div>
