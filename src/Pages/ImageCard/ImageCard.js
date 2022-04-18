1cdb34ec1f73441020aa3776955648e9aa08cc27
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-18 23:35:58 +0600

    fixed class to className

--- src/Pages/ImageCard/ImageCard.js
+++ src/Pages/ImageCard/ImageCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageCard = ({ image }) => {
     const tags = image.tags.split(',');
     return (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <img src={image.webformatURL} alt="Random" className="w-full" />
             <div className="px-6 py-4">
                 <div className="font-bold text-red-600 text-xl mb-2">Photo by Steve Rogers</div>
